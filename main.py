e5c46e9dab83b3205a3af75bb80cfe893715bbfc
ravulakolrajesh <rajesh.ravulakol@gmail.com>
2026-04-18 12:43:43 +0530

    29buildcommit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to DevOps using ngrok 2ndattempt"
+    return "Welcome to DevOps using ngrok 29ndattempt"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
